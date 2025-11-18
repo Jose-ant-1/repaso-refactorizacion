7aa416af04b94603e0094542a28c6539876cf27b
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-11-18 22:17:09 +0100

    Refactor index.js to use ES6 modules and fix bugs

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("La página ha cargado");
+console.log("La página ha cargado");
 
-    const NOTA_MAX = 10;
-    const NOTA_MIN = 0;
-    const NOTA_SUFICIENTE = 5;
-    const NOTA_NOTABLE = 6;
-    const NOTA_SOBRESALIENTE = 8;
+const NOTA_MAX = 10;
+const NOTA_MIN = 0;
+const NOTA_SUFICIENTE = 5;
+const NOTA_NOTABLE = 6;
+const NOTA_SOBRESALIENTE = 8;
 
-    let notas = [];
+export let notas = [];
 
-    function compruebaNota(nota) {
 
-        if (!nota.isNaN) {
-            console.log(`La nota ${nota} no es un número`);
-            return false;
-        }
-        if (nota < NOTA_MIN || nota > NOTA_MAX) {
-            console.log(`${nota}: fuera de rango`)
-            return false;
-        }
-        return true;
+export function insnertarNota(nota) {
+    if (nota.isNaN) {
+        console.log(`La nota ${nota} no es un número`);
+        return false;
+    }
+    if (nota < NOTA_MIN || nota > NOTA_MAX) {
+        console.log(`${nota}: fuera de rango`)
+        return false;
+    }
+    notas.push(nota);
+}
+
+export function calcularPromedio(notas) {
+    let total = 0;
+    const totalNotas = notas.reduce((acumulador, nota) => acumulador + nota, total);
+    return totalNotas / notas.length;
+}
+
+export function obtenerCalificacion(promedio) {
+    if (promedio < NOTA_SUFICIENTE) {
+        return {estado: "suspendido", calificacion: "suspenso"};
     }
 
-
-    function calcularPromedio(notas) {
-        let totalNotas = notas.reduce(nota => {
-            return totalNotas += nota
-        });
-        return totalNotas / notas.length;
+    if (promedio < NOTA_NOTABLE) {
+        return {estado: "aprobado", calificacion: "suficiente"};
     }
 
-    function obtenerCalificacion(promedio) {
-        if (promedio < NOTA_SUFICIENTE) {
-            return {estado: "suspendido", calificacion: "suspenso"};
-        }
-
-        if (promedio < NOTA_NOTABLE) {
-            return {estado: "aprobado", calificacion: "suficiente"};
-        }
-
-        if (promedio < NOTA_SOBRESALIENTE) {
-            return {estado: "aprobado", calificacion: "notable"};
-        }
-
-        return {estado: "aprobado", calificacion: "sobresaliente"};
+    if (promedio < NOTA_SOBRESALIENTE) {
+        return {estado: "aprobado", calificacion: "notable"};
     }
-})
+
+    return {estado: "aprobado", calificacion: "sobresaliente"};
+}
+
+console.log(insnertarNota(4));
+console.log(insnertarNota(5));
+console.log(insnertarNota(6));
+console.log(calcularPromedio(notas));
+
+
+
